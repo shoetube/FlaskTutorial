824f4bcaf737694dd53cf6117d024f01f75efd4b
shoetube <shoetube@hotmail.com>
2020-09-28 21:53:17 -0700

    Complete web forms tutorial chapter 3

---
+++ README.txt
@@ -0,0 +1,12 @@
+This tutorial comes from "http://blog.miguelgrinberg.com"
+To activate the virtual environment, use the following command:
+    source venv/bin/activate
+
+to run the program, enter:
+    flask run
+
+Requirements:
+    python 3.7
+    flask
+    flask-wtf
+
---
+++ app/routes.py
@@ -0,0 +1,32 @@
+from flask import render_template
+from flask import flash
+from flask import redirect
+from flask import url_for
+from app import app
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Scott'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Amie'},
+            'body': 'Feed me more tilapia!'
+        }
+    ]
+    return render_template(
+        'index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me{}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
